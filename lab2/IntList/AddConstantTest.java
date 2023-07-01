dc16e6ef8bb8fbd4f45d3172a579fec8eead0dab
Your Name <you@berkeley.edu>
2023-07-01 16:17:01 +0800

    completed first part of lab1

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -7,9 +7,9 @@ public class AddConstantTest {
 
     @Test
     public void testAddConstantOne() {
-        IntList lst = IntList.of(1, 2, 3, 4, 5);
-        IntListExercises.addConstant(lst, 1);
-        assertEquals("2 -> 3 -> 4 -> 5 -> 6", lst.toString());
+        IntList lst = IntList.of(1,2,3);
+        IntListExercises.addConstant(lst, -2);
+        assertEquals("-1 -> 0 -> 1", lst.toString());
     }
 
     @Test
