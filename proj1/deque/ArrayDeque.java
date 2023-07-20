a3f7d015918ffbc6ccc35605483b23d3e96b6101
Your Name <you@berkeley.edu>
2023-07-20 11:15:05 +0800

    update

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,6 +1,6 @@
 package deque;
 import java.util.Iterator;
-public class ArrayDeque<T> implements Deque<T> , Iterable<T> {
+public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     private int size;
 
     /*pos records the position of the first element backward*/
@@ -12,60 +12,66 @@ public class ArrayDeque<T> implements Deque<T> , Iterable<T> {
 
     public ArrayDeque() {
         array = (T[]) new Object[8];
-        size=0;
-        posback=7;
-        posfore=0;
-        totallength=8;
+        size = 0;
+        posback = 7;
+        posfore = 0;
+        totallength = 8;
     }
 
-    private void resizing(int capacity){
-        T[] temp=(T[]) new Object[capacity];
-        for(int i=0;i<posfore;i++){
-            temp[i]=array[i];
+    private void resizing(int capacity) {
+        T[] temp = (T[]) new Object[capacity];
+        for (int i = 0; i < posfore; i++) {
+            temp[i] = array[i];
         }
-        int j=capacity-1;
-        for(int i=totallength-1;i>posback;i--,j--){
-            temp[j]=array[i];
+        int j = capacity - 1;
+        for (int i = totallength - 1; i > posback; i--, j--) {
+            temp[j] = array[i];
         }
-        posback=j;
-        totallength=capacity;
-        array=temp;
+        posback = j;
+        totallength = capacity;
+        array = temp;
     }
 
     @Override
-    public void addFirst(T item){
-        if(item==null) return;
-        if(size==totallength){
-            resizing((int)(size*1.5));
+    public void addFirst(T item) {
+        if (item == null) {
+            return;
         }
-        array[posback]=item;
-        posback-=1;
-        size+=1;
-    }
-
-    @Override
-    public void addLast(T item){
-        if(item==null) return;
-        if(size==totallength){
-            resizing((int)(size*1.5));
+        if (size == totallength){
+            resizing((int)(size * 1.5));
         }
-        array[posfore]=item;
-        posfore+=1;
-        size+=1;
+        array[posback] = item;
+        posback -= 1;
+        size += 1;
     }
 
     @Override
-    public boolean isEmpty(){
-        return size==0;
+    public void addLast(T item) {
+        if (item == null) {
+            return;
+        }
+        if (size == totallength) {
+            resizing((int)(size * 1.5));
+        }
+        array[posfore] = item;
+        posfore += 1;
+        size += 1;
     }
 
     @Override
-    public int size(){return size;}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     @Override
-    public void printDeque(){
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<size-1;i++){
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size-1; i++) {
             sb.append(get(i));
             sb.append(' ');
         }
@@ -77,101 +83,117 @@ public class ArrayDeque<T> implements Deque<T> , Iterable<T> {
 
 
     @Override
-    public T removeFirst(){
-        if(isEmpty()) return null;
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
         T first;
-        if(posback!=totallength-1){
-            first=array[posback+1];
-            array[posback+1]=null;
-            size-=1;
-            posback+=1;
-        }else{
-            first=array[0];
-            posfore-=1;
-            for(int i=0;i<posfore;i++){
-                array[i]=array[i+1];
+        if (posback != totallength-1) {
+            first = array[posback+1];
+            array[posback+1] = null;
+            size -= 1;
+            posback += 1;
+        }else {
+            first = array[0];
+            posfore -= 1;
+            for(int i = 0; i < posfore; i++) {
+                array[i] = array[i+1];
             }
-            array[posfore]=null;
-            size-=1;
+            array[posfore] = null;
+            size -= 1;
         }
 
         //resize if necessary
-        if((size<totallength/4) && (totallength>=16)){
+        if ((size < totallength/4) && (totallength >= 16)) {
             resizing(totallength/4);
         }
         return first;
     }
 
     @Override
-    public T removeLast(){
-        if(isEmpty()) return null;
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
         T last;
-        if(posfore!=0){
-            last=array[posfore-1];
-            array[posfore-1]=null;
-            size-=1;
-            posfore-=1;
-        }else{
-            last=array[totallength-1];
-            posback+=1;
-            for(int i=totallength-1;i>posback;i--){
-                array[i]=array[i-1];
+        if (posfore != 0) {
+            last = array[posfore - 1];
+            array[posfore - 1] = null;
+            size -= 1;
+            posfore -= 1;
+        }else {
+            last = array[totallength-1];
+            posback += 1;
+            for(int i = totallength - 1; i > posback; i--) {
+                array[i] = array[i - 1];
             }
-            array[posback]=null;
-            size-=1;
+            array[posback] = null;
+            size -= 1;
         }
 
         //resize if necessary
-        if((size<totallength/4) && (totallength>=16)){
+        if ((size < totallength/4) && (totallength >= 16)) {
             resizing(totallength/4);
         }
         return last;
     }
 
     @Override
-    public T get(int index){
-        if((index>=0) && (index<totallength-posback-1)){
-            return array[index+posback+1];
-        }else if((index>=totallength-posback-1) && (index<size)){
+    public T get(int index) {
+        if ((index >= 0) && (index < totallength - posback - 1)) {
+            return array[index+posback + 1];
+        }else if ((index>=totallength-posback-1) && (index<size)){
             return array[index-(totallength-posback-1)];
         }
         return null;
     }
 
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new ArrayIterator();
     }
 
     @Override
-    public boolean equals(Object o){
-        if(this==o) return true;
-        if(o==null) return false;
-        if(!(o instanceof Deque)) return false;
+    public boolean equals(Object o) {
+        if (this==o) {
+            return true;
+        }
+        if (o==null) {
+            return false;
+        }
+        if (!(o instanceof Deque)) {
+            return false;
+        }
 
-        Deque<T> other=(Deque<T>) o;
-        if(this.size()!=other.size()) return false;
-        for(int i=0;i<size;i++){
-            if(this.get(i)!=other.get(i)) return false;
+        Deque<T> other = (Deque<T>) o;
+        if (this.size() != other.size()) {
+            return false;
+        }
+        for (int i = 0; i < size; i++){
+            if (this.get(i) != other.get(i)) {
+                return false;
+            }
         }
         return true;
     }
 
 
-    private class ArrayIterator implements Iterator<T>{
+    private class ArrayIterator implements Iterator<T> {
         private int CurPos;
         public ArrayIterator(){
             CurPos=0;
         }
         @Override
         public boolean hasNext(){
-            if(get(CurPos)!=null) return true;
+            if (get(CurPos) != null) {
+                return true;
+            }
             return false;
         }
 
         @Override
-        public T next(){
-            T returnItem=get(CurPos);
-            CurPos+=1;
+        public T next() {
+            T returnItem = get(CurPos);
+            CurPos += 1;
             return returnItem;
         }
     }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,151 +1,179 @@
 package deque;
 import java.util.Iterator;
-public class LinkedListDeque<T> implements Deque<T>,Iterable<T>{
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
     private int size;
     private StuffNode SentinelFront;
     private StuffNode SentinelBack;
 
-    private class StuffNode<T>{
+    private class StuffNode<T> {
         public T item;
         public StuffNode next;
         public StuffNode prev;
-        public StuffNode(T i, StuffNode N_next,StuffNode N_prev){
-            item=i;
-            next=N_next;
-            prev=N_prev;
+        public StuffNode(T i, StuffNode N_next, StuffNode N_prev){
+            item = i;
+            next = N_next;
+            prev = N_prev;
         }
     }
 
-    public LinkedListDeque(){
-        SentinelFront=new StuffNode(null,null,null);
-        SentinelBack=new StuffNode(null,null,SentinelFront);
-        SentinelFront.next=SentinelBack;
+    public LinkedListDeque() {
+        SentinelFront = new StuffNode(null,null,null);
+        SentinelBack = new StuffNode(null,null,SentinelFront);
+        SentinelFront.next = SentinelBack;
         size=0;
     }
 
     @Override
-    public void addFirst(T item){
-        if(item==null) return;
-        StuffNode temp=SentinelFront.next;
-        SentinelFront.next=new StuffNode(item,temp,SentinelFront);
-        temp.prev=SentinelFront.next;
-        size+=1;
+    public void addFirst(T item) {
+        if(item == null) {
+            return;
+        }
+        StuffNode temp = SentinelFront.next;
+        SentinelFront.next = new StuffNode(item,temp,SentinelFront);
+        temp.prev = SentinelFront.next;
+        size += 1;
     }
 
     @Override
     public void addLast(T item){
-        if(item==null) return;
-        StuffNode temp=SentinelBack.prev;
-        SentinelBack.prev=new StuffNode(item,SentinelBack,temp);
-        temp.next=SentinelBack.prev;
-        size+=1;
+        if (item==null) {
+            return;
+        }
+        StuffNode temp = SentinelBack.prev;
+        SentinelBack.prev = new StuffNode(item,SentinelBack,temp);
+        temp.next = SentinelBack.prev;
+        size += 1;
 
     }
 
     @Override
-    public boolean isEmpty(){
+    public boolean isEmpty() {
+
         return size==0;
     }
 
     @Override
-    public int size(){return size;}
+    public int size() {
+        return size;
+    }
 
     @Override
-    public void printDeque(){
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<size-1;i++){
+    public void printDeque() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < size - 1; i++) {
             sb.append(get(i));
             sb.append(" ");
         }
-        sb.append(get(size-1));
+        sb.append(get(size - 1));
         System.out.print(sb.toString());
         System.out.println();
     }
 
     @Override
-    public T removeFirst(){
-        if(isEmpty()) return null;
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
         T first;
-        first=(T) SentinelFront.next.item;
-        SentinelFront.next=SentinelFront.next.next;
+        first = (T) SentinelFront.next.item;
+        SentinelFront.next = SentinelFront.next.next;
         SentinelFront.next.prev=SentinelFront;
-        size-=1;
+        size -= 1;
         return first;
     }
 
     @Override
-    public T removeLast(){
-        if(isEmpty()) return null;
+    public T removeLast() {
+        if (isEmpty()) return null;
         T last;
-        last=(T) SentinelBack.prev.item;
-        SentinelBack.prev=SentinelBack.prev.prev;
-        SentinelBack.prev.next=SentinelBack;
-        size-=1;
+        last = (T) SentinelBack.prev.item;
+        SentinelBack.prev = SentinelBack.prev.prev;
+        SentinelBack.prev.next = SentinelBack;
+        size -= 1;
         return last;
     }
 
     @Override
-    public T get(int index){
-        if(index>=size) return null;
-        T getitem;
-        StuffNode temp=SentinelFront.next;
-        for(int i=0;i<index;i++){
-            temp=temp.next;
+    public T get(int index) {
+        if (index >= size) {
+            return null;
         }
-        getitem=(T) temp.item;
+        T getitem;
+        StuffNode temp = SentinelFront.next;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        getitem = (T) temp.item;
         return getitem;
     }
 
-    public T getRecursive(int index){
-        if(size==0) return null;
-        if(index>=size || index<0) return null;
-        StuffNode<T> CurPos=SentinelFront.next;
-        T result=Recursive(index,CurPos);
+    public T getRecursive(int index) {
+        if (size == 0) {
+            return null;
+        }
+        if (index >= size || index < 0) {
+            return null;
+        }
+        StuffNode<T> CurPos = SentinelFront.next;
+        T result = Recursive(index, CurPos);
         return result;
     }
 
-    private T Recursive(int index,StuffNode<T> CurPos){
-        if(index==0) return (T) CurPos.item;
-        return (T) Recursive(index-1,CurPos.next);
+    private T Recursive(int index, StuffNode<T> CurPos) {
+        if (index == 0) {
+            return (T) CurPos.item;
+        }
+        return (T) Recursive(index-1, CurPos.next);
     }
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new LinkedListIterator();
     }
 
-    private class LinkedListIterator implements Iterator<T>{
+    private class LinkedListIterator implements Iterator<T> {
         private int CurPos;
 
-        public LinkedListIterator()  {CurPos=0;}
+        public LinkedListIterator()  {
+            CurPos=0;}
         @Override
-        public boolean hasNext(){
-            if(get(CurPos)!=null) return true;
+        public boolean hasNext() {
+            if (get(CurPos) != null) {
+                return true;
+            }
             return false;
         }
 
         @Override
-        public T next(){
+        public T next() {
             T temp;
-            temp=get(CurPos);
-            CurPos+=1;
+            temp = get(CurPos);
+            CurPos += 1;
             return temp;
         }
 
     }
 
     @Override
-    public boolean equals(Object o){
-        if(this==o) return true;
-        if(o==null) return false;
-        if(!(o instanceof Deque)) return false;
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Deque)) {
+            return false;
+        }
 
-        Deque<T> other=(Deque<T>)  o;
-        if(this.size()!=other.size()) return false;
-        for(int i=0;i<size;i++){
-            if(this.get(i)!=other.get(i)) return false;
+        Deque<T> other = (Deque<T>)  o;
+        if (this.size() != other.size()) {
+            return false;
+        }
+        for (int i = 0;i < size; i++) {
+            if (this.get(i) != other.get(i)) {
+                return false;
+            }
         }
         return true;
         }
-
-
 
 }
