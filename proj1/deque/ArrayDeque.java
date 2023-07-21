5bbe82478c2075b48531ea18ec9331c46df38405
Your Name <you@berkeley.edu>
2023-07-21 17:36:32 +0800

    modify method equals

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -165,7 +165,7 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
             return false;
         }
         for (int i = 0; i < size; i++) {
-            if (this.get(i) != other.get(i)) {
+            if (!this.get(i).equals(other.get(i))) {
                 return false;
             }
         }
